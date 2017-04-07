31fef9a0000fa8f765612ba34561a80597523e68
brendan brown <brendanbrown27@gmail.com>
2017-04-07 14:36:15 +0200

    ht2

--- server.js
+++ server.js
@@ -4,93 +4,92 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var config = require('./config.js');
 var app = express();
-var googleKey = process.env.GOOGLE_CIVIC_INFO || config.apiKeys.googleCivicInfo;
+var googleKey = process.env.GOOGLE_CIVIC_INFO || require('./config.js').apiKeys.googleCivicInfo;
 
 app.set('port', process.env.NODE_PORT || process.env.PORT || 3000);
 app.set('host', process.env.NODE_IP || 'localhost');
 
 console.log("starting up");
 console.log(process.env.GOOGLE_CIVIC_INFO, process.env.MONGODB_URI);
-//
-// try {
-// 	mongoose.connect(process.env.MONGODB_URI || config.database.localUrl);
-// } catch (e) {
-// 	console.log("mongoose connect failed", e);
-// }
-//
-// app.use(morgan('dev')); // logs all requests to console
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.static('./public'));
-// app.use(express.static('/Users/Jaime/Desktop/brendan_codes/contact_congress'));
 
-//
-//
-// var repList = mongoose.model('Replist', {
-// 	text: { type: String, default: '' },
-// 	name: { type: String, default: '' }
-// });
-//
-// app.get("/lists/all", function (req, res) {
-// 	repList.find(function (err, lists) {
-// 		if (err) {
-// 			res.send({ status: 'error', message: 'trouble reading from the database' });
-// 		} else {
-// 			res.json(lists);
-// 		}
-// 	});
-// });
-//
-// app.get("/lists/:name", function (req, res) {
-// 	repList.find({ name: req.params.name }, function (err, list) {
-// 		if (err) {
-// 			res.send({ status: 'error', message: 'trouble reading from the database' });
-// 		} else if (!list || !list.length) {
-// 			res.send({ status: 'error', message: 'no list found' });
-// 		} else {
-// 			res.json(list);
-// 		}
-// 	});
-// });
-//
-// app.post('/lists/create', function (req, res) {
-// 	if (!req.body.name || !req.body.text) {
-// 		res.send({ status: 'error', message: 'fields cannot be blank' });
-// 	} else {
-// 		repList.find({ 'name': req.body.name }, function (err, list) {
-// 			if (err) {
-// 				res.send({ status: 'error', message: 'trouble reading from the database' });
-// 			} else if (!list || !list.length) {
-// 				repList.create({
-// 					text: req.body.text,
-// 					name: req.body.name
-// 				}, function (err, list) {
-// 					if (err) {
-// 						res.send({ status: 'error', message: 'trouble reading from the database' });
-// 					} else {
-// 						res.json({ status: 'success', message: 'success' });
-// 					}
-// 				});
-// 			} else {
-// 				res.send({ status: 'error', message: 'name already taken' });
-// 			}
-// 		});
-// 	}
-// });
-//
-// app.get("/localRep/:address", function (req, res) {
-// 	var url = [
-// 		'https://www.googleapis.com/civicinfo/v2/representatives?key=',
-// 		googleKey,
-// 		'&address=',
-// 		req.params.address
-// 	].join('');
-// 	var remote = request(url);
-// 	req.pipe(remote);
-// 	remote.pipe(res);
-// });
+try {
+	mongoose.connect(process.env.MONGODB_URI || require('./config.js').database.localUrl);
+} catch (e) {
+	console.log("mongoose connect failed", e);
+}
+
+app.use(morgan('dev')); // logs all requests to console
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+app.use(express.static('/Users/Jaime/Desktop/brendan_codes/contact_congress'));
+
+
+
+var repList = mongoose.model('Replist', {
+	text: { type: String, default: '' },
+	name: { type: String, default: '' }
+});
+
+app.get("/lists/all", function (req, res) {
+	repList.find(function (err, lists) {
+		if (err) {
+			res.send({ status: 'error', message: 'trouble reading from the database' });
+		} else {
+			res.json(lists);
+		}
+	});
+});
+
+app.get("/lists/:name", function (req, res) {
+	repList.find({ name: req.params.name }, function (err, list) {
+		if (err) {
+			res.send({ status: 'error', message: 'trouble reading from the database' });
+		} else if (!list || !list.length) {
+			res.send({ status: 'error', message: 'no list found' });
+		} else {
+			res.json(list);
+		}
+	});
+});
+
+app.post('/lists/create', function (req, res) {
+	if (!req.body.name || !req.body.text) {
+		res.send({ status: 'error', message: 'fields cannot be blank' });
+	} else {
+		repList.find({ 'name': req.body.name }, function (err, list) {
+			if (err) {
+				res.send({ status: 'error', message: 'trouble reading from the database' });
+			} else if (!list || !list.length) {
+				repList.create({
+					text: req.body.text,
+					name: req.body.name
+				}, function (err, list) {
+					if (err) {
+						res.send({ status: 'error', message: 'trouble reading from the database' });
+					} else {
+						res.json({ status: 'success', message: 'success' });
+					}
+				});
+			} else {
+				res.send({ status: 'error', message: 'name already taken' });
+			}
+		});
+	}
+});
+
+app.get("/localRep/:address", function (req, res) {
+	var url = [
+		'https://www.googleapis.com/civicinfo/v2/representatives?key=',
+		googleKey,
+		'&address=',
+		req.params.address
+	].join('');
+	var remote = request(url);
+	req.pipe(remote);
+	remote.pipe(res);
+});
 
 app.get('*', function (req, res) {
 	res.redirect('/');
